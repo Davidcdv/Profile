2a91f443d23f0f476143c4247fcd77212793b970
Mac <mac@Macs-MacBook-Pro.local>
2024-11-27 23:55:18 +0100

    Set outDir to 'dist' in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Define the output directory for Vite's build
+  },
+});
